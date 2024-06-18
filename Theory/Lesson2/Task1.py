cb2d2c1d66c20ae6208f50ef8447d9e2c3333533
eldar <kastoom204@gmail.com>
2024-06-18 21:45:16 +0500

    Lesson2

---
+++ Theory/Lesson2/Task1.py
@@ -0,0 +1,78 @@
+# list_1 = []
+# list_1 = list()
+# list_1 = [1,2,3,4]
+# # print(*list_1)
+
+# for i in list_1:
+#     print(i)
+# list_1 = [1,2,3,4]
+# print(*list_1)
+# list_1.append(8)
+# print(*list_1)
+
+# list = []
+
+# for i in range(5):
+#     list.append(i)
+#     print(list)
+
+# list_1 = [12, 7, -1, 21, 0]
+# print(list_1.pop()) # 0 
+# print(list_1) # [12, 7, -1, 21]
+# print(list_1.pop()) # 21
+# print(list_1) # [12, 7, -1]
+# print(list_1.pop()) # -1
+# print(list_1) # [12, 7]
+# t = ()
+# print(type(t))
+
+# t = (1,2,2,5,)
+# print(type(t))
+
+# v = [1, 8, 9]
+# print(v)
+# print(type(v))
+
+# v = tuple(v)
+# print(v)
+# print(type(v))
+
+
+
+# a,b,c = v
+# print(a,b,c)
+
+# t = (1,2,3,5)
+
+# for i in t:
+#     print(i)
+
+# t = (1,2,3,5)
+
+# for i in range(len(t)):
+#     print(t[i])
+
+# t = [1,2,3,5]
+
+# t[0] = 2
+# print(t)
+
+# d = {}
+# d = dict()
+
+# d['q'] = 'qwerty'
+# # print(d)
+
+# d['w'] = 'werty'
+# print(d['q'])
+
+
+# a = {1, 2, 3, 5, 8}
+# b = {2, 5, 8, 13, 21}
+# c = a.copy() # c = {1, 2, 3, 5, 8}
+# u = a.union(b) # u = {1, 2, 3, 5, 8, 13, 
+# i = a.intersection(b) # i = {8, 2, 5}
+# dl = a.difference(b) # dl = {1, 3}
+# dr = b.difference(a) # dr = {13, 21}
+# q = a.union(b).difference(a.intersection(b))# {1, 21, 3, 13}
+
